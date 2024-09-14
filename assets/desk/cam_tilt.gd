962b7fbac7a72a2eb394cc48f88ebe665442603b
josephsmithberger <josephsmithberger@gmail.com>
2024-09-14 10:28:22 -0400

    mouse property
    prep before adding story

--- assets/desk/cam_tilt.gd
+++ assets/desk/cam_tilt.gd
@@ -1,12 +1,14 @@
 extends Camera3D
 
-#intensity of rotation effect
 @export var intensity: float = 0.1
+@export var ease_speed: float = 5.0
 
 var original_rotation: Vector3
+var target_rotation: Vector3
 
 func _ready():
 	original_rotation = rotation_degrees
+	target_rotation = rotation_degrees
 
 func _process(delta):
 	var mouse_pos = get_viewport().get_mouse_position()
@@ -15,4 +17,8 @@ func _process(delta):
 		-mouse_pos.x * intensity, 
 		0
 	)
-	rotation_degrees = original_rotation + offset_rotation
+	target_rotation = original_rotation + offset_rotation
+	
+	rotation_degrees.x = lerp_angle(rotation_degrees.x, target_rotation.x, delta * ease_speed)
+	rotation_degrees.y = lerp_angle(rotation_degrees.y, target_rotation.y, delta * ease_speed)
+	rotation_degrees.z = lerp_angle(rotation_degrees.z, target_rotation.z, delta * ease_speed)
---
+++ assets/puzzles/pickable_property.gd
@@ -0,0 +1,7 @@
+extends Area3D
+
+
+func _on_input_event(camera: Node, event: InputEvent, event_position: Vector3, normal: Vector3, shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed == true:
+			print("gay")
